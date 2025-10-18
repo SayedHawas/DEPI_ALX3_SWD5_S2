7f1fbb5d6017cac9150f2c8cacffeda6ca046642
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-10-18 16:20:05 +0300

    update

--- LINQDemo/Program.cs
+++ LINQDemo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, DEPI_ALX3_SWD5_S2 !");
+            Console.ReadLine();
         }
     }
 }
