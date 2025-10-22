96819fab609c128de3ad24d00bf7529c660cd691
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-10-22 20:55:32 +0300

    Update

---
+++ Day2EFCoreCodeFirstDemo/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day2EFCoreCodeFirstDemo.Models
+{
+    public class Student
+    {
+        //Data Annontation  Attributes --> Configuration Database 
+        [Key]
+        public int StudentId { get; set; }  //Primary Key + Identity (1,1)
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime JoinDate { get; set; }
+        public int? Mark { get; set; }
+
+        public override string ToString()
+        {
+            return $"student Id : {StudentId} Name : {Name} Date : {JoinDate}";
+        }
+    }
+}
