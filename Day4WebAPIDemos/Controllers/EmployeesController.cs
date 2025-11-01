67c8a838385fef749661cb1a87ad243ef0ce28ba
Sayed Hawas <39957108+SayedHawas@users.noreply.github.com>
2025-11-01 20:47:28 +0200

    Update

---
+++ Day4WebAPIDemos/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day4WebAPIDemos.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        public static List<string> Names = new List<string> { "Ahmed", "Tamer", "Yasser", "Mariem" };
+
+        //https://localhost:7299/api/Employees 
+        [HttpGet]
+        public List<string> Get()
+        {
+            return Names.ToList();
+        }
+        //https://localhost:7299/api/Employees/2
+        [HttpGet("{id}")]
+        public string GetById(int id)
+        {
+            if (id > Names.Count)
+            {
+                return "Not Found";
+            }
+            else
+                return Names[id];
+        }
+
+
+
+    }
+}
